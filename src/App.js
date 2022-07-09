4663a896805972f44366338162b925ff8b328402
rahadislam <rahadislm.web@gmil.com>
2022-07-09 20:41:18 +0600

    header area implement

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import Breakfast from "./Components/Pages/Home/FoodItems/Breakfast";
 import Dinner from "./Components/Pages/Home/FoodItems/Dinner";
 import Lunch from "./Components/Pages/Home/FoodItems/Lunch";
+import Header from "./Components/Pages/Home/Header/Header";
 import Home from "./Components/Pages/Home/Home";
 
 function App() {
     return (
         <div className="App">
+            <Header></Header>
             <Routes>
                 <Route path="/" element={<Home />}>
                     <Route path="breakfast" element={<Breakfast />}></Route>

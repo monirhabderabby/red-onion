a3514090cecd41c6346c595af0a95080d49ac48b
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-07-11 07:19:18 +0600

    resolve code conflict

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Breakfast from "./Components/Pages/Home/FoodItems/Breakfast";
+import Dinner from "./Components/Pages/Home/FoodItems/Dinner";
+import Lunch from "./Components/Pages/Home/FoodItems/Lunch";
+import Header from "./Components/Pages/Home/Header/Header";
+import Home from "./Components/Pages/Home/Home";
+import Login from "./Components/Pages/Home/Login/Login/Login";
+import SignUp from "./Components/Pages/Home/Login/SignUp/SignUp";
 
 function App() {
     return (
@@ -8,9 +15,9 @@ function App() {
             <Routes>
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<SignUp />} />
-=======
+                =======
                 <Route path="/" element={<Home />}>
-                    <Route path="breakfast" element={<Breakfast />}></Route>
+                    <Route path="/" element={<Breakfast />}></Route>
                     <Route path="lunch" element={<Lunch />}></Route>
                     <Route path="dinner" element={<Dinner />}></Route>
                 </Route>
